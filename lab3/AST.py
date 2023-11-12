9d672a3e0f2a4093b2333684afb1e4c5ce3e8100
IwoSzczepaniak <iszczepaniak@student.agh.edu.pl>
2023-11-13 00:44:38 +0100

    lab3_done

---
+++ lab3/AST.py
@@ -0,0 +1,177 @@
+class Node:
+    def __init__(self, **kwargs):
+        self.__dict__.update(kwargs)
+
+    def print_indent(self, i):
+        print(i * "|\t", end="")
+
+
+class InstructionsOrEmpty(Node):
+    def __init__(self, instructions=None):
+        super().__init__(instructions=instructions)
+    def printTree(self, i=0):
+        self.instructions.printTree(i)
+
+class Instructions(Node):
+    def __init__(self, instructions):
+        super().__init__(instructions=instructions)
+    def printTree(self, i=0):
+        for instruction in self.instructions:
+            instruction.printTree(i)
+
+
+class IfStatement(Node):
+    def __init__(self, cond, if_body, else_body=None):
+        super().__init__(cond=cond, if_body=if_body, else_body=else_body)
+    def printTree(self, i):
+        self.print_indent(i)
+        print("IF")
+        self.cond.printTree(i+1)
+        self.print_indent(i)
+        print("THEN")
+        self.if_body.printTree(i+1)
+        if self.else_body is not None:
+            self.print_indent(i)
+            print("ELSE")
+            self.else_body.printTree(i+1)
+
+class ReturnStatement(Node):
+    def __init__(self, expr=None):
+        super().__init__(expr=expr)
+    def printTree(self, i):
+        self.print_indent(i)
+        print("RETURN")
+        if self.expr is not None:
+            self.expr.printTree(i+1)
+
+class BreakStatement(Node):
+    def printTree(self, i):
+        self.print_indent(i)
+        print("BREAK")
+
+class ContinueStatement(Node):
+    def printTree(self, indent):
+        self.print_indent(i)
+        print("CONTINUE")
+
+class ForLoop(Node):
+    def __init__(self, id, cond_start, cond_end, body):
+        super().__init__(id=id, cond_start=cond_start, cond_end=cond_end, body=body)
+    def printTree(self, i):
+        self.print_indent(i)
+        print("FOR")
+        self.id.printTree(i+1)
+        self.print_indent(i+1)
+        print("RANGE")
+        self.cond_start.printTree(i+2)
+        self.cond_end.printTree(i+2)
+        self.body.printTree(i+1)
+
+class WhileLoop(Node):
+    def __init__(self, cond, body):
+        super().__init__(cond=cond, body=body)
+    def printTree(self, i):
+        self.print_indent(i)
+        print("WHILE")
+        self.cond.printTree(i+1)
+        self.body.printTree(i+1)
+
+class Assignment(Node):
+    def __init__(self, left, op, right):
+        super().__init__(left=left, op=op, right=right)
+    def printTree(self, i):
+        self.print_indent(i)
+        self.left.printTree(i)
+        self.print_indent(i+1)
+        print(self.op)
+        self.right.printTree(i+2)
+
+class PrintStatement(Node):
+    def __init__(self, printargs):
+        super().__init__(printargs=printargs)
+    def printTree(self, i):
+        self.print_indent(i)
+        print("PRINT")
+        for printarg in self.printargs:
+            printarg.printTree(i+1)
+
+
+class StringLiteral(Node):
+    def __init__(self, string):
+        super().__init__(string=string)
+    def printTree(self, i):
+        self.print_indent(i)
+        print("STRING")
+        self.print_indent(i+1)
+        print(self.string)
+
+class IntNum(Node):
+    def __init__(self, intnum):
+        super().__init__(intnum=intnum)
+    def printTree(self, i ):
+        self.print_indent(i)
+        print(self.intnum)
+
+class Float(Node):
+    def __init__(self, floatnum):
+        super().__init__(floatnum=floatnum)
+    def printTree(self, i ):
+        self.print_indent(i)
+        print("FLOAT")
+        self.print_indent(i+1)
+        print(self.floatnum)
+
+class Variable(Node):
+    def __init__(self, id, index=None):
+        super().__init__(id=id, index=index)
+    def printTree(self, i):
+        if self.index is not None:
+            self.print_indent(i)
+            print("REF")
+            self.id.printTree(i+1)
+            for e in self.index:
+                e.printTree(i+1)
+
+class Id(Node):
+    def __init__(self, id):
+        super().__init__(id=id)
+    def printTree(self, i):
+        self.print_indent(i)
+        print(self.id)
+
+class BinaryExpression(Node):
+    def __init__(self, left, op, right):
+        super().__init__(left=left, op=op, right=right)
+    def printTree(self, i):
+        self.left.printTree(i)
+        self.print_indent(i+1)
+        print(self.op)
+        self.right.printTree(i+2)
+
+class Matrix(Node):
+    def __init__(self, matrix):
+        super().__init__(matrix=matrix)
+    def printTree(self, i):
+        self.print_indent(i)
+        print("VECTOR")
+        for row in self.matrix:
+            self.print_indent(i + 1)
+            print("VECTOR")
+            for expr in row:
+                expr.printTree(i+2)
+
+class MatFun(Node):
+    def __init__(self, func, expr):
+        super().__init__(func=func, expr=expr)
+    def printTree(self, i):
+        self.print_indent(i)
+        print(self.func)
+        self.expr.printTree(i+1)
+
+class UnaryExpression(Node):
+    def __init__(self, operation, expr):
+        super().__init__(operation=operation, expr=expr)
+    def printTree(self, i):
+        self.print_indent(i)
+        print(self.operation)
+        self.expr.printTree(i+1)
---
+++ lab3/MyParserAST.py
@@ -0,0 +1,199 @@
+from sly import Parser
+import sys
+sys.path.append('c:/Users/iwosz/PythonProjects/CompilationTheory/') 
+from lab1.MyLexer import MyLexer as Scanner
+from AST import *
+
+class ParserMatrixAST(Parser):
+
+    tokens = Scanner.tokens
+
+    precedence = (
+        ('right', 'IF', 'ELSE'),
+        ('right', 'FOR', 'WHILE'),
+        ('left', '+', '-'),
+        ('left', '*', '/'),
+        ('left', 'DOTADD', 'DOTSUB'),
+        ('left', 'DOTMUL', 'DOTDIV'),
+        ('right', '=', 'ADDASSIGN', 'SUBASSIGN', 'MULASSIGN', 'DIVASSIGN'),
+        ('nonassoc', 'LT', 'GT', 'EQ', 'NE', 'GE', 'LE'),
+        ('left', 'TRANSPOSE'),
+        ('left', 'UMINUS'),
+    )
+
+    @_('instructions_or_empty')
+    def program(self, p):
+        return InstructionsOrEmpty(p[0])
+
+    @_('instructions')
+    def instructions_or_empty(self, p):
+        return Instructions(p[0])
+
+    @_('')
+    def instructions_or_empty(self, p):
+        return Instructions()
+
+    @_('instructions instruction',
+       'instruction')
+    def instructions(self, p):
+        return p[0] + p[1] if len(p) == 2 else p[0]
+
+    @_('if_i',
+       'return_i ";"',
+       'BREAK ";"',
+       'CONTINUE ";"',
+       'for_l',
+       'while_l',
+       'assign ";"',
+       'print_i ";"')
+    def instruction(self, p):
+        return [p[0]]
+    
+    @_('"{" instructions "}"')
+    def instruction(self, p):
+        return p[1]
+
+    @_('IF "(" expr ")" instruction %prec IF',
+       'IF "(" expr ")" instruction ELSE instruction')
+    def if_i(self, p):
+        return IfStatement(p[2], Instructions(p[4])) if len(p) == 5 else IfStatement(p[2], Instructions(p[4]), Instructions(p[6]))
+
+
+    @_('WHILE "(" expr ")" instruction' )
+    def while_l(self, p):
+        return WhileLoop(p[2], Instructions(p[4]))
+
+    @_('FOR ID "=" expr ":" expr instruction')
+    def for_l(self, p):
+        return ForLoop(Id(p[1]), p[3], p[5], Instructions(p[6]))
+
+
+    @_('RETURN',
+       'RETURN expr')
+    def return_i(self, p):
+        return ReturnStatement(p[1]) if len(p) == 2 else ReturnStatement()
+
+
+    @_('PRINT printargs')
+    def print_i(self, p):
+        return PrintStatement(p[1])
+
+    @_('expr "," printargs',
+        'expr')
+    def printargs(self, p):
+        return [p[0]] + p[2] if len(p) == 3 else [p[0]]
+
+
+    @_('STRING')
+    def expr(self, p):
+        return StringLiteral(p[0])
+
+    @_('INTNUM')
+    def expr(self, p):
+        return IntNum(p[0])
+
+    @_('FLOAT')
+    def expr(self, p):
+        return Float(p[0])
+
+    @_('var')
+    def expr(self, p):
+        return p[0]
+    @_('"(" expr ")"')
+    def expr(self, p):
+        return p[1]
+
+    @_('lists')
+    def var(self, p):
+        return p[0]
+
+    @_('ID')
+    def var(self, p):
+        return Id(p[0])
+
+    @_('ID "[" expr "," expr "]"')
+    def lists(self, p):
+        return Variable(Id(p[0]), (p[2], p[4]))
+
+    @_('var "=" expr',
+       'var ADDASSIGN expr',
+       'var SUBASSIGN expr',
+       'var MULASSIGN expr',
+       'var DIVASSIGN expr')
+    def assign(self, p):
+        return Assignment(p[0], p[1], p[2])
+
+    @_('"-" expr %prec UMINUS',
+        '''expr "'" %prec TRANSPOSE''')
+    def expr(self, p):
+        return UnaryExpression(p[1], p[0])
+
+    @_('matrix')
+    def expr(self, p):
+        return p[0]
+
+    @_('expr "+" expr',
+       'expr "-" expr',
+       'expr "*" expr',
+       'expr "/" expr',
+
+       'expr EQ expr',
+       'expr NE expr',
+       'expr LT expr',
+       'expr GT expr',
+       'expr LE expr',
+       'expr GE expr',
+
+       'expr DOTMUL expr',
+       'expr DOTDIV expr',
+       'expr DOTADD expr',
+       'expr DOTSUB expr',
+       )
+    def expr(self, p):
+        return BinaryExpression(p[0], p[1], p[2])
+
+
+
+    @_('"[" vectors "]"')
+    def matrix(self, p):
+        return Matrix(p[1])
+
+    @_('vectors "," vector',
+       'vector')
+    def vectors(self, p):
+        return p[0] + [p[2]] if len(p) == 3 else [p[0]]
+
+    @_('"[" variables "]"')
+    def vector(self, p):
+        return p[1]
+
+
+    @_('variables "," variable',
+       'variable')
+    def variables(self, p):
+        return  p[0] + [p[2]] if len(p) == 3 else [p[0]]
+
+    @_('expr')
+    def variable(self, p):
+        return p[0]
+
+
+    @_('mat_fun "(" expr ")"')
+    def expr(self, p):
+        return MatFun(p[0], p[2])
+
+
+    @_('ZEROS',
+       'EYE',
+       'ONES')
+    def mat_fun(self, p):
+        return p[0]
+
+
+    def error(self, p):
+        if p:
+            print("Syntax error at line {0}: LexToken({1}, '{2}')".format(p.lineno, p.type, p.value))
+        else:
+            print("Unexpected end of input")
+
+
